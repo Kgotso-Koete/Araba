f6ea715a23975b434d929dd178ac770ca71d3956
“Kgotso-Koete” <kgkoete@gmail.com>
2019-07-08 21:00:15 +0200

    Commit 4-2:Added data to the posts collection

---
+++ server/fixtures.js
@@ -0,0 +1,19 @@
+Meteor.startup(() => {
+  // code to run on server at startup
+  if (Posts.find().count() === 0) {
+    Posts.insert({
+      title: 'Introducing Telescope',
+      url: 'http://sachagreif.com/introducing-telescope/'
+    });
+  
+    Posts.insert({
+      title: 'Meteor',
+      url: 'http://meteor.com'
+    });
+  
+    Posts.insert({
+      title: 'The Meteor Book',
+      url: 'http://themeteorbook.com'
+    });
+  }  
+}); 
