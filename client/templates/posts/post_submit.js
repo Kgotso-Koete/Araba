702d203a7638b7820460fea2cce1ac758ff4cde8
“Kgotso-Koete” <kgkoete@gmail.com>
2019-07-10 04:07:41 +0200

    Commit 7-6:Use a method to submit the post

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,14 +1,18 @@
 Template.postSubmit.events({
-    'submit form': function(e) {
-      e.preventDefault();
-  
-      var post = {
-        url: $(e.target).find('[name=url]').val(),
-        title: $(e.target).find('[name=title]').val()
-      };
-  
-      post._id = Posts.insert(post);
-      Router.go('postPage', post);
-    }
-  });  
-  
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    Meteor.call('postInsert', post, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+
+      Router.go('postPage', {_id: result._id});  
+    });
+  }
+});
