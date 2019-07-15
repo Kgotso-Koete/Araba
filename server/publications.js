646b9a183967d6b25e4960121c57bc6b17c52da9
“Kgotso-Koete” <kgkoete@gmail.com>
2019-07-15 15:22:52 +0200

    Commit 14-1:Added post reordering animation

--- server/publications.js
+++ server/publications.js
@@ -15,7 +15,7 @@ Meteor.publish('comments', function(postId) {
   check(postId, String);
   return Comments.find({postId: postId});
 });
-
+ 
 Meteor.publish('notifications', function() {
   return Notifications.find({userId: this.userId, read: false});
 });  
